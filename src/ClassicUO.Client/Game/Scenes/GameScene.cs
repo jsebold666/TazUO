1f0c18c7ab0751a331587de196b2cafc0559f8ae
Outkome <kai.luu1988@gmail.com>
2023-11-26 02:07:20 +1030

    Test

--- src/ClassicUO.Client/Game/Scenes/GameScene.cs
+++ src/ClassicUO.Client/Game/Scenes/GameScene.cs
@@ -8,7 +8,7 @@
 // 1. Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 // 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
+//    notice, this list of conditions and the following disclaimer in the// test
 //    documentation and/or other materials provided with the distribution.
 // 3. All advertising materials mentioning features or use of this software
 //    must display the following acknowledgement:
