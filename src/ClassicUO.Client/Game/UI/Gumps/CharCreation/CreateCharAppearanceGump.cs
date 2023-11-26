f5fc1e0f9a0a954bf413db88b30e5a28dd7172f3
Outkome <kai.luu1988@gmail.com>
2023-11-27 01:28:30 +1030

    NO GARGZ

--- src/ClassicUO.Client/Game/UI/Gumps/CharCreation/CreateCharAppearanceGump.cs
+++ src/ClassicUO.Client/Game/UI/Gumps/CharCreation/CreateCharAppearanceGump.cs
@@ -204,7 +204,7 @@ namespace ClassicUO.Game.UI.Gumps.CharCreation
                 1
             );
 
-            if (Client.Version >= ClientVersion.CV_60144)
+            /*if (Client.Version >= ClientVersion.CV_60144)
             {
                 Add
                 (
@@ -223,7 +223,7 @@ namespace ClassicUO.Game.UI.Gumps.CharCreation
                     },
                     1
                 );
-            }
+            }*/
 
             // Prev/Next
             Add
